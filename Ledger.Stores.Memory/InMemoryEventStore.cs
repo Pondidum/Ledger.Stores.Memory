da5707653f429f30aba762067af559b9bc2cafeb
andy <andy@andydote.co.uk>
2015-06-27 23:21:09 +0100

    stores.memory: extract fakeEventStore to separate project

---
+++ Ledger.Stores.Memory/InMemoryEventStore.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Ledger.Stores.Memory
+{
+	public class InMemoryEventStore : IEventStore
+	{
+		public int? LatestSequenceID { get; set; }
+		public int? LatestSnapshotID { get; set; }
+		public List<object> Events { get; set; }
+		public ISequenced Snapshot { get; set; }
+
+		public InMemoryEventStore()
+		{
+			Events = new List<object>();
+			Events = new List<object>();
+			LatestSequenceID = null;
+			Snapshot = null;
+		}
+
+		public int? GetLatestSequenceFor<TKey>(TKey aggegateID)
+		{
+			return LatestSequenceID;
+		}
+
+		public int? GetLatestSnapshotSequenceFor<TKey>(TKey aggregateID)
+		{
+			return LatestSnapshotID;
+		}
+
+		public void SaveEvents<TKey>(TKey aggegateID, IEnumerable<DomainEvent> changes)
+		{
+			Events.AddRange(changes);
+		}
+
+		public IEnumerable<DomainEvent> LoadEvents<TKey>(TKey aggegateID)
+		{
+			return Events.Cast<DomainEvent>();
+		}
+
+		public IEnumerable<DomainEvent> LoadEventsSince<TKey>(TKey aggegateID, int sequenceID)
+		{
+			return Events.Cast<DomainEvent>().Where(x => x.SequenceID > sequenceID);
+		}
+
+		public ISequenced LoadLatestSnapshotFor<TKey>(TKey aggegateID)
+		{
+			return Snapshot;
+		}
+
+		public void SaveSnapshot<TKey>(TKey aggregateID, ISequenced snapshot)
+		{
+			Snapshot = snapshot;
+		}
+	}
+}
